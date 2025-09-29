6c1da80c17952e4ce4c22d9c2178265ce7b4e974
Aleksandr Gurov <aleksandr.gurov2605@gmail.com>
2025-09-29 11:39:02 +0700

    finish 5.5, add relationship in models

--- app/models/products.py
+++ app/models/products.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String, Boolean, Float, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Float, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
@@ -14,3 +14,7 @@ class Product(Base):
     image_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
+
+    category: Mapped["Category"] = relationship(back_populates="products")
+
